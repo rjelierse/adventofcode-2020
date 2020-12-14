3874b6622f3526905383be55b685014517d64ea5
Raymond Jelierse <raymond@messagebird.com>
2020-12-14 17:02:52 +0100

    feat(day13): solution, part 2

--- 13/index.js
+++ 13/index.js
@@ -16,3 +16,23 @@ const {line, departsIn} = busLines
     .reduce((line, table) => line !== null && line.departsIn < table.departsIn ? line : table, null)
 
 console.log(`Bus ${line} will departs in ${departsIn} minutes: ${line * departsIn}`)
+
+const ts = () => {
+    let [multiplier] = busLines;
+    const busses = busLines
+        .map((line, index) => ({line, offset: index}))
+        .filter(({line}) => line !== 'x');
+
+    return busses.slice(1).reduce((ts, {line, offset}) => {
+        while (true) {
+            if ((ts + offset) % line === 0) {
+                multiplier *= line;
+                break;
+            }
+            ts += multiplier;
+        }
+        return ts;
+    }, 0)
+}
+
+console.log(`First timestamp for matching schedules is ${ts()}`)
